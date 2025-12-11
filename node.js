381f27773e109f3e9a623016b6f9624c9df3d222
john-h-repository <lnpirrotta@email.neit.edu>
2025-12-11 12:48:47 -0500

    asdadsasdasd

--- node.js
+++ node.js
@@ -42,7 +42,10 @@ for (let i = 0; i < 750; i++) {
 }
 
 const skybox = new Image()
-skybox.src = '.
+skybox.src = './textures/skybox.png'
+let skyboxX = 0
+let skyboxSpeed = .5
+
 function drawSkybox() {
     if (!skybox.complete) return
 
